c304f5574388867e0ad341c33b8f49fce6311174
Luis Miguel <lmavendano1597@gmail.com>
2023-01-29 22:41:14 -0500

    first-commit

---
+++ src/lots/dto/create-lot.dto.ts
@@ -0,0 +1,32 @@
+import { IsString, IsNumber, IsPositive, IsOptional } from 'class-validator';
+
+export class CreateLotDto {
+  @IsString()
+  code: string;
+
+  @IsNumber()
+  @IsPositive()
+  quantityLiters: number;
+
+  @IsNumber()
+  @IsOptional()
+  discount?: number;
+
+  @IsString()
+  @IsOptional()
+  discountReason?: string;
+
+  @IsString()
+  typeLot: string;
+
+  @IsNumber()
+  @IsPositive()
+  costLiter: number;
+
+  @IsNumber()
+  @IsOptional()
+  reusedBottles?: number;
+
+  @IsNumber()
+  lotTotalCost: number;
+}
